7d90fb7780816da9b1b06145b87b63bac63f4a79
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:59:48 +0200

    returns the length of a string

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
