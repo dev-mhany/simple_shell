bf94c174b18939352ee3df698a2f063564adb861
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:50:03 +0200

    prints an input string

---
+++ _error_puts.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ *_error_puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _error_puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_error_putchar(str[i]);
+		i++;
+	}
+}
