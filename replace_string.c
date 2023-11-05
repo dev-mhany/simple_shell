32b53ba7736fbf0dcfba80ee6602b4101f6d3f6d
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:16:01 +0200

    replaces string

---
+++ replace_string.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * replace_string - replaces string
+ * @old: address of old string
+ * @new: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
