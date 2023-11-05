56d3a18677a85ad127e20ec94a77249109f2c9f5
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:17:33 +0200

    blocks ctrl-C

---
+++ sigintHandler.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
