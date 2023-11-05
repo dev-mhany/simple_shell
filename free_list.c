973e6914e59d56f1157e885e61af520b37f9264a
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:08:28 +0200

    frees all nodes of a list

---
+++ free_list.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head_ptr: address of pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t **head_ptr)
+{
+	list_t *node, *next_node, *head;
+
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
+}
