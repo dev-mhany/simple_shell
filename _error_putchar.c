6bf5fd94290549d72a861219e288f788119014c1
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:49:27 +0200

    writes the character c to stderr

---
+++ _error_putchar.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _error_putchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _error_putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
