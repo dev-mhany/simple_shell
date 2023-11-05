2290e3ada2b6a757bf13353a85cc188cfee20110
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:00:16 +0200

    concatenates two strings.

---
+++ _strncat.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
