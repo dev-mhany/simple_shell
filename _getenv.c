b6d5ec6c3cf98142512890ef84ebb0ad86cbf2e2
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:51:10 +0200

    gets the value of an environ variable

---
+++ _getenv.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
