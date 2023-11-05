490c0248941c44469d1f1727d080bd3aba36e4a7
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:54:01 +0200

    writes the character c to stdout

---
+++ _putchar.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
