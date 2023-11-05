4828e509f53512f1fd074e59af15a659b46728d2
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:10:29 +0200

    buffers chained commands

---
+++ input_buffer.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * input_buffer - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buffer(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+
+		r = _getline(info, buf, &len_p);
+
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			comment_handling(*buf);
+			*len = r;
+			info->cmd_buf = buf;
+		}
+	}
+	return (r);
+}
