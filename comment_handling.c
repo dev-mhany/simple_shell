5a12ebe82c72f6439fecf36a445d7c928848a6e7
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:05:18 +0200

    function replaces first instance of '#' with '\0'

---
+++ comment_handling.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * comment_handling - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void comment_handling(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
