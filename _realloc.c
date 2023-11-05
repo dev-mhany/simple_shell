c899729d7b2671db0c5ff7762dc9a9d35b803bf3
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:56:13 +0200

    reallocates a block of memory

---
+++ _realloc.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to old block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to new block, or NULL on failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
