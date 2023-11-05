945fe005fce5440f14d736b057219aa65806a47b
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:56:48 +0200

    Initialize a new environment variable

---
+++ _setenv.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *			 or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *		constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_error_puts("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *			 or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *		constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
