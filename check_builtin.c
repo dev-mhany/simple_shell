be093fcb7caf8ffbd7a21a06a34fa6ca19941fe9
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:03:40 +0200

    finds a builtin command.

---
+++ check_builtin.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * check_builtin - finds a builtin command
+ * @info: the parameter & return info struct
+ *
+ * Return: -1 if builtin not found,
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
+ */
+int check_builtin(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", exit_shell},
+		{"env", _env},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _change_dir},
+		{"alias", _myalias},
+		{NULL, NULL}};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
