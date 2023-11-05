240a34dd90367106231c296c26e358078f1304f2
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:09:16 +0200

    returns the string array copy of our environ

---
+++ get_environ.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *		  constant function prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
