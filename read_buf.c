7b7bace92f243ea9ef60974ff5014f025dcfc4b0
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:15:38 +0200

    reads a buffer

---
+++ read_buf.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
