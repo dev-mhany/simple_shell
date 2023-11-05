f31fd336dbb67c1287eed533b6e2cb7aefb12a2e
Muhammad Hany <dev.mhany@gmail.com>
2023-11-05 23:55:08 +0200

    prints an input string

---
+++ _puts.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
