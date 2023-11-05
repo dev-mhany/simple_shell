ab3ffa38dd7e1f459b9b1c92f90bc1ae32b02405
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:04:30 +0200

    checks should continue chaining based last status.

---
+++ check_next.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * check_next - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
+ * Return: Void
+ */
+void check_next(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->cmd_buf_type == AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+
+	*p = j;
+}
