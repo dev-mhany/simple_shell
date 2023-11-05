b8284bac0043cea3e9052e0e4a0ec86c4fb3b19f
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:12:16 +0200

    test if current char in buffer is aChain delimeter

---
+++ is_next.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * is_next - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
+int is_next(info_t *info, char *buf, size_t *p)
+{
+	size_t j = *p;
+
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = OR;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = AND;
+	}
+	else if (buf[j] == ';') /* found end of this command */
+	{
+		buf[j] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CHAIN;
+	}
+	else
+		return (0);
+	*p = j;
+	return (1);
+}
