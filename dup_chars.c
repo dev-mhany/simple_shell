aa1c54026734469b6e6f09b4c6e593d10efd97c0
Muhammad Hany <dev.mhany@gmail.com>
2023-11-06 00:06:04 +0200

    duplicates characters

---
+++ dup_chars.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
