dd5b742b15dbcb90b42efc56b738e8f2cc7ffeef
wafaamohamed1 <wm172302@gmail.com>
2023-11-14 20:11:40 +0200

    determines if a file is an executable command

--- is_cmd.c
+++ is_cmd.c
@@ -4,7 +4,7 @@
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
- *
+ *wm
  * Return: 1 if true, 0 otherwise
  */
 int is_cmd(info_t *info, char *path)
