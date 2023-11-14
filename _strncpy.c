80cab3770f2394e823ceb227c7974c88d37c6c6b
wafaamohamed1 <wm172302@gmail.com>
2023-11-14 20:02:41 +0200

    copies a string

--- _strncpy.c
+++ _strncpy.c
@@ -5,6 +5,7 @@
  *@dest: the destination string to be copied to
  *@src: the source string
  *@n: the amount of characters to be copied
+ *wm
  *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
