d6c9ba05eb5f4ec369cd6068c128db274f8b603d
wafaamohamed1 <wm172302@gmail.com>
2023-11-14 20:07:33 +0200

    exits the shell

--- exit_shell.c
+++ exit_shell.c
@@ -4,6 +4,7 @@
  * exit_shell - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *		  constant function prototype.
+ *wm
  *  Return: exits with a given exit status
  *		 (0) if info.argv[0] != "exit"
  */
