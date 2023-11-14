ae0bacedd6eead95da74b413edfe1a08296a83e0
wafaamohamed1 <wm172302@gmail.com>
2023-11-14 20:12:18 +0200

    test if current char in buffer

--- is_next.c
+++ is_next.c
@@ -5,7 +5,7 @@
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
- *
+ *wm
  * Return: 1 if chain delimeter, 0 otherwise
  */
 int is_next(info_t *info, char *buf, size_t *p)
